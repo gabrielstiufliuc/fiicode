760821765ddffb9205a1c45d8627fa57118faeab
Gabriel Stiufliuc <gabriel.stiufliuc6@gmail.com>
2019-03-16 16:48:00 +0200

    Add root files

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { Route, Switch } from './routing';
+import * as React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import { withRouter } from 'react-router';
+
+EStyleSheet.build({});
+
+interface Props {
+    history: any;
+    location: any;
+}
+
+interface State {
+    isAuth: boolean;
+}
+
+class App extends React.Component<Props, State> {
+    constructor(props: any, context: any) {
+        super(props, context);
+
+        this.state = {
+            isAuth: false,
+        } as State;
+    }
+
+    public render() {
+        return (
+            <View nativeID={'app'}>
+                <SafeAreaView nativeID={'safeAreaView'} style={{ flex: 1 }}>
+                    <Switch>
+                        <Route render={() => <Text>Hello</Text>} path='/' />
+                    </Switch>
+                </SafeAreaView>
+            </View>
+        );
+    }
+}
+
+// in order to access history from props, use withRouter HOC
+export default withRouter<any>(App);
---
+++ src/routing.tsx
@@ -0,0 +1,10 @@
+import { createBrowserHistory } from 'history';
+
+export {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from 'react-router-dom';
+
+export let history = createBrowserHistory();
